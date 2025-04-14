cad53177d2021ff12f4b9f92fbe80ba10ee2bcf6
viniAmorim <amorimm.viniciuss@gmail.com>
2025-04-14 16:35:05 -0300

    chore: basic structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import userRoute from './routes';
+
+const app = express()
+
+const PORT = 3333;
+
+app.use(express.json())
+app.use(userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
