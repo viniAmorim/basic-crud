f1ead3b2994ec7406d1cb2b290094ad7cb279a92
viniAmorim <amorimm.viniciuss@gmail.com>
2025-04-14 23:08:31 -0300

    refactor: separate app for server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
-import express from 'express';
-import userRoute from './routes';
-
-const app = express()
+import { app } from './app';
 
 const PORT = 3333;
 
-app.use(express.json())
-app.use(userRoute);
-
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
