26ac3475400ff27913a841c4e024fbe8dc0efc14
viniAmorim <amorimm.viniciuss@gmail.com>
2025-04-15 18:39:35 -0300

    feat: container and user route

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { router } from './routes';
+
+import '@shared/container';
+import { AppError } from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 export { app };
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import { CreateUserController } from '../../../../modules/accounts/useCases/createUser/CreateUserController';
 
 const usersRoutes = Router();
+
+const createUserController = new CreateUserController();
 
 usersRoutes.get('/', (req, res) => {
   res.json({ message: 'Hello user router' });
 });
+
+usersRoutes.post('/', createUserController.handle.bind(createUserController));
 
 export { usersRoutes };
