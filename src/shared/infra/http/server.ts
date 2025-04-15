e9353fcd96580afc259fe31b133d9a46a5517618
viniAmorim <amorimm.viniciuss@gmail.com>
2025-04-15 18:29:16 -0300

    feat: add app data source to server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,12 @@
+import 'reflect-metadata';
+import { AppDataSource } from '@shared/infra/typeorm/data-source';
 import { app } from './app';
 
 const PORT = 3333;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => console.log('🚀 Server running on port 3333'));
+  })
+  .catch((error) =>
+    console.error('❌ Data Source initialization error', error),
+  );
