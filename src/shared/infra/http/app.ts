1105f86b974ad48bf0c4a4e2b9c9e9274c789181
viniAmorim <amorimm.viniciuss@gmail.com>
2025-04-20 19:03:17 -0300

    feat: swagger documentation

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes';
 
+import swaggerUi from "swagger-ui-express";
+
+import swaggerFile from '../../../../swagger.json'
+
 import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
